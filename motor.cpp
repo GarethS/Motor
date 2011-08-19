cecacbde98b428ff68ae0ae920d064bf418ef7b2
GaryS <garethjscott@gmail.com>
2011-08-18 18:16:31 -0700

    Add logging.

--- motor.cpp
+++ motor.cpp
@@ -8,11 +8,14 @@
 */
 
 #include "../../../../DriverLib/boards/ek-lm3s3748/motor/stepper.h"
+//#include "log.h"
 
 #include <iostream>
 using namespace std;
 
 int main(void) {
+	//logc l("TESTa");
+	//l.dump("start motor");
 	stepper s; 
 	s.dryRunAccel();
 	
