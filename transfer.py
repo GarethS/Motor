4c2a55890d44710b35c7fb71e311f08d2533096f
GaryS <garethjscott@gmail.com>
2013-05-30 14:11:20 -0700

    Adding Python program to transfer compiled program and sysmbol table to controller.  Simple lex/yacc parser supplied as a Python import module which could be useful later on for parsing more complicated messages returned from controller. This transfer program has not been tested on hardware yet.

---
+++ transfer.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2013 Gareth Scott
+# transfer.py
+
+# See: http://pyserial.sourceforge.net/pyserial_api.html#module-serial
+import serial
+import lex
+import yacc
+import datetime as dt
+
+# Globals
+currentToken    = 0
+
+# List of token names.   This is always required
+tokens = (
+    'TOKEN_START',
+    'TOKEN_END',
+    'OK',
+    'ERR',
+)
+
+# Regular expression rules for simple tokens
+t_TOKEN_START       = r'<'
+t_TOKEN_END         = r'>'
+t_OK                = r'OK'
+t_ERR               = r'ERR'
+
+def t_NUMBER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print "Integer too large:", t.value
+        t.value = 0
+    return t
+
+# Error handling rule
+def t_error(t):
+    #print "Illegal character:'%s'" % t.value[0]
+    t.lexer.skip(1)
+
+lex.lex()
+
+def p_OK(t):
+    'expression : TOKEN_START OK TOKEN_END'
+    global currentCount
+    currentToken = 1
+
+def p_ERR(t):
+    'expression : TOKEN_START ERR TOKEN_END'
+    global currentCount
+    currentToken = 2
+
+def p_error(t):
+    print "Syntax error at:'%s'" % t.value
+    while True:
+        tok = yacc.token()             # Get next token
+        if not tok or tok.type == 'TOKEN_START': break
+    yacc.restart()
+
+yacc.yacc()
+    
+class Transfer():
+    def __init__(self, fileName):
+        self.__fileName = fileName
+        self.__lineCount = 0
+        self.__haveSerialPort = False
+        if self.__haveSerialPort:
+            self.__ser = serial.Serial(
+                port     = 0,
+                baudrate = 9600,
+                bytesize = serial.EIGHTBITS,
+                parity   = serial.PARITY_NONE,
+                stopbits = serial.STOPBITS_ONE,
+                timeout  = 1,
+                xonxoff  = 0,
+                rtscts   = 0
+            )
+            print "Serial port:", self.__ser.isOpen()
+            print self.__ser
+        
+        self.cmdOK =            "<OK>"
+        self.cmdErr =           "<ERR>"
+        
+        self.parseStr = ""
+
+    def send(self):
+        f = open(self.__fileName)
+        for line in iter(f):
+            if self.sendLine(line) == False:
+                break
+        f.close()
+        
+    def sendLine(self, line):
+        print 'Sending:{0}[{1}]'.format(self.__lineCount, line)
+        self.__lineCount = self.__lineCount + 1
+        if self.__haveSerialPort:
+            self.__ser.write(line)
+            self.parseInputTimeout()
+            # read input, allow delay
+            # if <ERR> resend x2 then exit with fatal error
+        return True
+
+    def parseInputTimeout(self):
+        # 1. Get current time
+        startTime = currentTime = dt.datetime.now()
+        while (currentTime - startTime).microseconds < 100000: 
+            # 2. Call parseInput
+            parseInput()
+            # 3. Got OK return True; got ERR, return False
+            if currentToken == OK:
+                return True
+            currentTime = dt.datetime.now()
+            # 4. If timer not expired, goto 2 above
+        return False
+        
+    def parseInput(self):
+        waitingCount = self.__ser.inWaiting()
+        #waitingCount = 1
+        if waitingCount == 0:
+            return
+        x = self.__ser.read(waitingCount)
+        self.parseStr += x
+        while len(self.parseStr) >= 1:
+            print "before:", self.parseStr
+            foundEndTokenIndex = self.parseStr.find(">")
+            if foundEndTokenIndex != -1:
+                # Found '>' token delimiter
+                newTokenString = self.parseStr[:foundEndTokenIndex+1]
+                foundBeginTokenIndex = newTokenString.rfind("<")
+                if foundBeginTokenIndex != -1 and foundBeginTokenIndex < foundEndTokenIndex:
+                    yaccInput = newTokenString[foundBeginTokenIndex:]
+                    print "yacc:", yaccInput
+                    currentToken = 0
+                    yacc.parse(yaccInput, debug=1)
+                # Remove up to end of token from string
+                self.parseStr = self.parseStr[foundEndTokenIndex+1:]
+                print "after:", self.parseStr
+            else:
+                break
+            
+if __name__ == "__main__":
+    t = Transfer("..\parser\symbolTable.txt")
+    t.send()
