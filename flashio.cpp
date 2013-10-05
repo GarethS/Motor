ea724ca1a3d4b330a77b3c40cad2fd7677bf2b32
GaryS <garethjscott@gmail.com>
2013-10-04 17:54:35 -0700

    Saving and restoring program to flash memory tested for first 3 entries in the interpret::_program[] table. See matching submission in parser repository. Current thinking is to add a button to the board that would transfer the program from flash to sRAM and begin executing. Another thought is to keep the program table, but not the symbol table (since it changes values), in flash all the time. That is a future optimization. No testing done on symbol table, but that should be straightforard. Note that one entry in _program[] takes up 12 bytes. In _symbolTable[] it's 16 bytes.

--- flashio.cpp
+++ flashio.cpp
@@ -1,5 +1,5 @@
 /*
-	Copyright (c) Gareth Scott 2011, 2012
+	Copyright (c) Gareth Scott 2013
 
 	flashio.cpp 
 
@@ -11,28 +11,77 @@
 #include "hw_memmap.h"
 #include "hw_types.h"
 #include "driverlib/rom.h"
-#include "driverlib/flash.h"   // Using "flash.h" pulls in file from FreeRTOS
+#include "driverlib/flash.h"   // Using "flash.h" pulls file from FreeRTOS. Not what we want.
 #include "flashio.h"
 
 flashio::flashio() {
 }
 
-void flashio::saveData(uint32_t* pData, unsigned long intCount/* 4 bytes per integer on 32-bit system */) {
-    uint32_t* remainingDataPointer = pData;
+// Move data from sRAM to Flash
+bool flashio::saveData(uint32_t* pDataSource, uint32_t flashDestination, unsigned long intCount/* 4 bytes per integer on 32-bit ARM systems */) {
+    uint32_t* remainingDataPointer = pDataSource;
     unsigned long remainingByteCount = intCount * sizeof(uint32_t);
     unsigned int pageCount = 0;
     while (remainingByteCount > 0) {
-        // TODO - Before erasing, check to see if the current flash page is identical to data to be programmed.
-        //         If so, no need to program, just skip over this page.
-        ROM_FlashErase(FLASH_START_ADDRESS + (pageCount * FLASH_PAGE_SIZE));
+        uint32_t flashAddress = flashDestination + (pageCount * FLASH_PAGE_SIZE);
+        // An optimization here would be if the flash memory contains the same contents as 'remainingDataPointer'
+        //  then we don't need to call ROM_FlashProgram() below.
+        if (_eraseBlockRequired(flashAddress, remainingDataPointer, remainingByteCount)) {
+            ROM_FlashErase(flashAddress);
+            if (!_flashErased(flashAddress)) {
+                return false;
+            }
+        }
         if (remainingByteCount >= FLASH_PAGE_SIZE) {
-            ROM_FlashProgram(remainingDataPointer, FLASH_START_ADDRESS + (pageCount * FLASH_PAGE_SIZE), FLASH_PAGE_SIZE);
+            ROM_FlashProgram(remainingDataPointer, flashAddress, FLASH_PAGE_SIZE);
             remainingDataPointer += FLASH_PAGE_SIZE;
             remainingByteCount -= FLASH_PAGE_SIZE;
         } else {
-            ROM_FlashProgram(remainingDataPointer, FLASH_START_ADDRESS + (pageCount * FLASH_PAGE_SIZE), remainingByteCount);
+            ROM_FlashProgram(remainingDataPointer, flashAddress, remainingByteCount);
             break;            
         }
         ++pageCount;
     }
+    return true;
+}
+
+// Move data from Flash into sRAM
+void flashio::retrieveData(uint32_t* pDataDestination, uint32_t flashSource, unsigned long intCount) {
+    uint32_t* pFlashAddress = (uint32_t*)flashSource;
+    for (uint32_t i = 0; i < intCount; ++i) {
+        pDataDestination[i] = pFlashAddress[i];
+    }
+}
+
+// Return false if flash block is all 0xff (i.e. erased) or data to be programmed matches data in flash block; true otherwise
+bool flashio::_eraseBlockRequired(uint32_t flashAddress, uint32_t* remainingDataPointer, unsigned long byteCount) {
+    if (byteCount >= FLASH_PAGE_SIZE) {
+        byteCount = FLASH_PAGE_SIZE;
+    }
+    uint32_t intCount = byteCount / sizeof(uint32_t);
+    uint32_t* pFlashAddress = (uint32_t*)flashAddress;
+    for (uint32_t i = 0; i < intCount; ++i) {
+        // Note, since pFlashAddress points to a 32-bit integer, we're incrementing
+        //  over 4 bytes each time through this loop
+        uint32_t flashValue = pFlashAddress[i];
+        if (!(flashValue == FLASH_ERASED_VALUE || flashValue == remainingDataPointer[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Called after ROM_FlashErase() to check that flash erased correctly. If bits are 
+//  starting to wear out this should detect it.
+// Return false if flash bad; true otherwise
+bool flashio::_flashErased(uint32_t flashAddress) {
+    uint32_t intCount = FLASH_PAGE_SIZE / sizeof(uint32_t);
+    uint32_t* pFlashAddress = (uint32_t*)flashAddress;
+    for (uint32_t i = 0; i < intCount; ++i) {
+        uint32_t flashValue = pFlashAddress[i];
+        if (flashValue != FLASH_ERASED_VALUE) {
+            return false;
+        }
+    }
+    return true;
 }
--- flashio.h
+++ flashio.h
@@ -10,15 +10,26 @@
 #ifndef _FLASHIO_H_
 #define _FLASHIO_H_
 
-#define FLASH_PAGE_SIZE (0x400) // 1024
-#define FLASH_START_ADDRESS (0xF000)    // Current code is located in flash at ~0x7200
+enum flashRegion {FLASH_PROGRAM, FLASH_SYMBOL_TABLE};
+
+#define FLASH_PAGE_SIZE                     (0x400) // 1024
+// Note: 0xff00 - 0xf000 = 0xf00 (3840) 32-bit spaces for program
+//       0xffff - 0xff00 = 0xff (256) 32-bit spaces for symbol table.
+#define FLASH_START_ADDRESS_PROGRAM         (0xF000)    // Current code is located in flash at ~0x7200
+#define FLASH_START_ADDRESS_SYMBOL_TABLE    (0xFF00)
+#define FLASH_ERASED_VALUE                  (0xFFFFFFFF)
 
 class flashio {
 public:
     flashio();
     ~flashio() {}
       
-    void saveData(uint32_t* pData, unsigned long intCount);  
+    bool saveData(uint32_t* pDataSource, uint32_t flashDestination, unsigned long intCount);
+    void retrieveData(uint32_t* pDataDestination, uint32_t flashSource, unsigned long intCount);
+  
+private:    
+    bool _eraseBlockRequired(uint32_t flashAddress, uint32_t* remainingDataPointer, unsigned long byteCount);
+    bool _flashErased(uint32_t flashAddress);
 };
 
 #endif /* _FLASHIO_H_ */
